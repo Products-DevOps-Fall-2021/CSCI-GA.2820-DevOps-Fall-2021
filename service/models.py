8f3e68421e26626ba612e1c233475087005a233c
Sumit Mamtani <sumitmamtani@sumits-mbp.myfiosgateway.com>
2021-10-23 19:51:33 -0400

    added 2 more APIs

--- service/models.py
+++ service/models.py
@@ -3,18 +3,14 @@ from flask.globals import request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
-
 db = SQLAlchemy()
-
 class ProductModel(db.Model):
-    # __tablename__ = 'products'
     app = None
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
-    #description = db.Column(db.String(80), nullable=False)
+    description = db.Column(db.String(80), nullable=False)
     creation_date = db.Column(db.DateTime, default=datetime.utcnow)#, nullable=False)
     price = db.Column(db.Float, nullable = False)
-
 
     def __repr__(self):
         return '<Task %r>' %self.id
@@ -27,16 +23,15 @@ class ProductModel(db.Model):
         db.create_all()  
 
     def save_to_db(new_product):
-        print("enter to save to db")
         db.session.add(new_product)
-        print("added not commited")
         db.session.commit()
-        print(" commited")
-
 
     def delete_from_db(request_id):
         db.session.delete(request_id)
         db.session.commit()
+
+    def get_products():
+        return ProductModel.query.order_by(ProductModel.creation_date).all()
 
     @classmethod        
     def find_by_name(cls,name):
@@ -46,7 +41,5 @@ class ProductModel(db.Model):
     def find_by_id(cls,id):
         return cls.query.filter_by(id=id).first()
 
-
 def init_db(app):
-    
     ProductModel.init_db(app)
--- service/products.py
+++ service/products.py
@@ -6,13 +6,13 @@ class ProductService():
 
     def index_page():
         products = ProductModel.query.order_by(ProductModel.creation_date).all()
-        print("sumit")
         return products
 
+    def get_all_products():
+        return ProductModel.get_products()
     
-    def create_product(product_name, product_price):
-        new_product = ProductModel(name = product_name, price = product_price)
-        print("after new_product")
+    def create_product(product_name, product_price, pdescription):
+        new_product = ProductModel(name = product_name, price = product_price, description = pdescription)
         try:
             ProductModel.save_to_db(new_product)
             return True   
@@ -21,23 +21,22 @@ class ProductService():
 
     def delete_product( id):
         product_to_delete = ProductModel.find_by_id(id)
-        print("find deleted_product")
         try:
             ProductModel.delete_from_db(product_to_delete)
-            print(" deleted")
             return True
         except Exception:
             return False
 
-    def update_product(id, name , price):
+    def update_product(id, name , price, description):
         product_to_update = ProductModel.find_by_id(id)
         try:
             product_to_update.name = name
             product_to_update.price = price
+            product_to_update.description = description
             ProductModel.save_to_db(product_to_update)
-            return True
+            return product_to_update
         except Exception:
             return False
     
     def find_product_by_id(id):
-        return ProductModel.find_by_id(id);
+        return ProductModel.find_by_id(id)
