3ab9e2de6fda1d7aae153025e8273be4aa4f63be
Sumit Mamtani <sumitmamtani@sumits-mbp.myfiosgateway.com>
2021-10-24 02:56:15 -0400

    added error handling

--- service/models.py
+++ service/models.py
@@ -2,8 +2,15 @@ from flask import Flask, render_template, url_for
 from flask.globals import request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from sqlalchemy.exc import InvalidRequestError
 
 db = SQLAlchemy()
+
+
+class DataValidationError(Exception):
+    """ Used for an data validation errors when deserializing """
+    pass
+
 class ProductModel(db.Model):
     app = None
     id = db.Column(db.Integer, primary_key=True)
@@ -23,12 +30,19 @@ class ProductModel(db.Model):
         db.create_all()  
 
     def save_to_db(new_product):
-        db.session.add(new_product)
-        db.session.commit()
+        try:
+            db.session.add(new_product)
+            db.session.commit()
+        except InvalidRequestError:
+            db.session.rollback()
+
 
     def delete_from_db(request_id):
         db.session.delete(request_id)
-        db.session.commit()
+        try:
+            db.session.commit()
+        except InvalidRequestError:
+            db.session.rollback()
 
     def get_products():
         return ProductModel.query.order_by(ProductModel.creation_date).all()
@@ -40,6 +54,30 @@ class ProductModel(db.Model):
     @classmethod        
     def find_by_id(cls,id):
         return cls.query.filter_by(id=id).first()
+
+    def serialize(self):
+        """ Serializes a Product into a dictionary """
+        return {
+            "id": self.id, 
+            "name": self.name,
+            "description": self.description,
+            "creation_date": self.creation_date,
+            "price": self.price
+        }
+
+    def deserialize(self, data):
+        """
+        Deserialize a Product from a dictionary
+        Args:
+            data (dict): A dictionary containing the resource data
+        """
+        self.name = data["name"]
+        self.description = data["description"]
+        self.price = data["price"]
+        self.id = data["id"]
+        self.creation_date = data["creation_date"]
+
+        return self
 
 def init_db(app):
     ProductModel.init_db(app)
--- service/routes.py
+++ service/routes.py
@@ -4,6 +4,31 @@ from werkzeug.utils import redirect
 from flask.globals import request
 from service.products import ProductService
 from service import app
+from service import status
+from flask_restx import Api
+
+authorizations = {
+    'apikey': {
+        'type': 'apiKey',
+        'in': 'header',
+        'name': 'X-Api-Key'
+    }
+}
+
+
+api = Api(app,
+          version='1.0.0',
+          title='Product Demo REST API Service',
+          description='This is a sample server Product store server.',
+          default='products',
+          default_label='Product shop operations',
+          authorizations=authorizations,
+         )
+
+def abort(error_code: int, message: str):
+    """Logs errors before aborting"""
+    app.logger.error(message)
+    api.abort(error_code, message)
 
 #root Home page
 @app.route("/",  methods=['GET'])
@@ -11,17 +36,22 @@ def index():
     products = ProductService.index_page()
     return render_template('index.html', products = products)
 
-# @app.route("/products", methods=["GET"])
-# def list_all_products(): 
-#     app.logger.info("Request to list all products") 
-#     products = ProductService.get_all_products()
-#     return products
+@app.route("/products", methods=["GET"])
+def list_all_products(): 
+    app.logger.info("Request to list all products") 
+    products = ProductService.get_all_products()
+    return products , status.HTTP_200_OK
 
-# @app.route("/products/<int:id>", methods=["GET"])
-# def list_product(id):
-#     app.logger.info("Request to list a product with a given id") 
-#     product= ProductService.find_product_by_id(id)
-#     return product
+@app.route("/products/<int:id>", methods=["GET"])
+def list_product(id):
+    try:
+        id = int(id)
+    except ValueError:
+        app.logger.info("Invalid Product ID.")
+        abort(status.HTTP_400_BAD_REQUEST, "Invalid Product ID.")
+    app.logger.info("Request to list a product with a given id") 
+    product= ProductService.find_product_by_id(id)
+    return product, status.HTTP_200_OK
 
 
 @app.route("/products", methods=["POST"])
@@ -30,20 +60,29 @@ def create():
     product_name = request.form['name']
     product_price = request.form['price']
     description = request.form['description']
+    if product_name is None:
+        abort(status.HTTP_417_EXPECTATION_FAILED, "Product name is required.")
+    if product_price is None or product_price<=0:
+        abort(status.HTTP_417_EXPECTATION_FAILED, "Product price is required.")
     output = ProductService.create_product(product_name, product_price, description)
     if output == True:
         return  redirect('/')
 
-    return "Issue adding product"
+    return abort(status.HTTP_503_SERVICE_UNAVAILABLE, "Product could not be added in db")
 
 
 @app.route('/delete/<int:id>', methods=["POST"])
 def delete(id):
+    try:
+        id = int(id)
+    except ValueError:
+        app.logger.info("Invalid Product ID.")
+        api.abort(status.HTTP_400_BAD_REQUEST, "Invalid Product ID.")
     app.logger.info("Request to delete product...")
     output = ProductService.delete_product(id)
     if output == True:
         return  redirect('/')
-    return 'There was a problem deleting the product'
+    abort(status.HTTP_404_NOT_FOUND, "Product with id '{}' was not found.".format(id))
 
 
 @app.route('/update/<int:id>', methods=['GET', 'POST'])
@@ -54,9 +93,11 @@ def update(id):
         description = request.form['description']
         output = ProductService.update_product(id,name, price, description)
         if output == False:
-            return 'Issue in updating product' 
+            abort(status.HTTP_503_SERVICE_UNAVAILABLE, "Product could not be updated with id '{}' was not found.".format(id))
         else :
             return redirect('/')
     else:
         product = ProductService.find_product_by_id(id)
+        if not product:
+            abort(status.HTTP_404_NOT_FOUND, "Product with id '{}' was not found.".format(id))
         return render_template('update.html', product=product)
