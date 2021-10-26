ad43169c4f357feecb1d0f38355ee6e2fe753c7f
Sumit Mamtani <sumitmamtani@sumits-mbp.myfiosgateway.com>
2021-10-25 22:38:36 -0400

    added schema design

--- service/models.py
+++ service/models.py
@@ -1,13 +1,8 @@
-from flask import Flask, render_template, url_for
-from flask.globals import request
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-from sqlalchemy.exc import InvalidRequestError
 
 db = SQLAlchemy()
-class DataValidationError(Exception):
-    """ Used for an data validation errors when deserializing """
-    pass
 class ProductModel(db.Model):
     app = None
     id = db.Column(db.Integer, primary_key=True)
@@ -25,53 +20,6 @@ class ProductModel(db.Model):
         db.init_app(app)
         app.app_context().push()
         db.create_all()  
-
-    def save_to_db(new_product):
-        try:
-            db.session.add(new_product)
-            db.session.commit()
-        except InvalidRequestError:
-            db.session.rollback()
-
-
-    def delete_from_db(request_id):
-        db.session.delete(request_id)
-        try:
-            db.session.commit()
-        except InvalidRequestError:
-            db.session.rollback()
-
-    def get_products():
-        return ProductModel.query.order_by(ProductModel.creation_date).all()
-
-    @classmethod        
-    def find_by_name(cls,name):
-        product = cls.query.filter_by(name=name).first()
-        return product
-        
-    
-    @classmethod        
-    def find_by_id(cls,id):
-        product = cls.query.filter_by(id=id).first()
-        return product
-
-    def serialize(self):
-        return {
-            "id": self.id, 
-            "name": self.name,
-            "description": self.description,
-            "creation_date": self.creation_date,
-            "price": self.price
-        }
-
-    def deserialize(self, data):
-        self.name = data["name"]
-        self.description = data["description"]
-        self.price = data["price"]
-        self.id = data["id"]
-        self.creation_date = data["creation_date"]
-
-        return self
 
 def init_db(app):
     ProductModel.init_db(app)
