23db097e0981be4358b9f2324d8d31acc495812f
Maitreya Sonawane <maitreyassonawane@gmail.com>
2021-10-26 12:12:17 -0400

    Delete API created with test cases and README updated

--- run.py
+++ run.py
@@ -5,5 +5,4 @@ DEBUG = (os.getenv('DEBUG', 'False') == 'True')
 PORT = os.getenv('PORT', '5000')
 
 if __name__ == "__main__":
-    app.logger.info("************RUN HERE*******")
     app.run(host='0.0.0.0', port=int(PORT), debug=DEBUG)
